b326e22e7297626b2bc6f96db50d91fa363dcb69
HarshaVardhan Developer <harshavardhan96310@gmail.com>
2024-12-24 11:42:36 +0530

    file

---
+++ class1.py
@@ -0,0 +1,65 @@
+# print("hello world")
+# a=input("enter the name")
+# print(a)
+# print(7 | 9)
+# print(8*"*")
+# print(7*"*")
+# print(6*"*")
+# print(5*"*")
+# print(4*"*")
+
+# print(3*"*")
+# print(2*"*")
+# print(1*"*")
+
+# print(bin(~20))
+
+# print(8 | 9)
+
+# print(8*9)
+
+# print(9<<8)
+# print(567>>98)
+
+# name = "PUSHPA"
+# if name == "PUSHPA":
+#     print("give me a chocolate")
+
+# number=-10
+# if number<0:
+#     print("number is negative")
+# else:
+#     print("number is positive")
+
+
+# a=int(input("enter the number"))
+# if a<0:
+#      print("number is negative")
+# elif a>0:
+#     print("number is positive")
+# elif a==0:
+#     print("number is zero")
+# else:
+#      print("number is positive")
+
+# for i in range(35,55):
+#     print(i)  # prints numbers from 0 through 12for i in range(13):
+#     if i/2:
+#         print(i,"even")  # prints numbers from 1 through 13
+#     else:
+#         print(i,"odd")
+
+# list=[1,2,3,4,5,5,5,5,7,8]
+# for i in list:
+#     print(i,"is present in the list")  # prints numbers from 1 through 13
+#     if i==5:
+#         print("number is available")
+
+
+
+f
+
+
+
+
+
